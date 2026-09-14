74b527ca567cd1405828df79309da62f39bb9136
iVolt1 <71899823+iVolt1@users.noreply.github.com>
2026-09-14 05:51:55 -0500

    typo

--- airplay_multiroom_audio/generate_airplay_services.sh
+++ airplay_multiroom_audio/generate_airplay_services.sh
@@ -109,9 +109,9 @@ sessioncontrol :
 pa :
 {
   sink = "${current_sink}";
-  application_name = "Shairport Sync";ystem log.
-  //output_rate = "192000";
-  //output_format = "S32_LE";
+  application_name = "Shairport Sync";
+  // output_rate = "192000";
+  // output_format = "S32_LE";
 };
 EOF
 
