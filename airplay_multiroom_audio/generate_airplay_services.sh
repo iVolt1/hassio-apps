5395837fc366a4e7807fa28f34f1b89c1b865704
iVolt1 <71899823+iVolt1@users.noreply.github.com>
2026-09-14 05:48:13 -0500

    auto rates

--- airplay_multiroom_audio/generate_airplay_services.sh
+++ airplay_multiroom_audio/generate_airplay_services.sh
@@ -110,8 +110,8 @@ pa :
 {
   sink = "${current_sink}";
   application_name = "Shairport Sync";ystem log.
-  output_rate = "192000";
-  output_format = "S32_LE";
+  //output_rate = "192000";
+  //output_format = "S32_LE";
 };
 EOF
 
