7ac008347b34520d9032dcf17dc53f578d141d6d
iVolt1 <71899823+iVolt1@users.noreply.github.com>
2026-09-21 16:14:53 -0500

    Remove underscores and improve Diagnostics

--- airplay2_multiroom_audio/generate_airplay2_services.sh
+++ airplay2_multiroom_audio/generate_airplay2_services.sh
@@ -205,7 +205,7 @@ while read -r sink; do
 
     UDP_PORT_BASE=$((UDP_PORT_BASE + 10))
     current_sink=$sink
-    current_name=$friendly_name
+    current_name="${friendly_name//_/}"
     current_log=$player_log
     current_udp_base=$UDP_PORT_BASE
 
@@ -225,8 +225,8 @@ sessioncontrol :
 };
 diagnostics :
 {
-//  log_show_time_since_startup = "yes";
-//  log_show_time_since_last_message = "no";
+  log_show_time_since_startup = "yes";
+  log_show_time_since_last_message = "no";
 };
 pulseaudio :
 {
